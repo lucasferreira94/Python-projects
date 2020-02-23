43c97d2b5863e705e335cf70e05b65cc0d9ad20c
lucasferreira94 <45995734+lucasferreira94@users.noreply.github.com>
2020-02-23 10:36:33 -0300

    Add files via upload
    Jogo da velha em Python

---
+++ jogo_da_velha.py
@@ -0,0 +1,44 @@
+'''
+JOGO DA VELHA
+'''
+
+# ABAIXO ESTAO AS POSICOES DA CERQUILHA
+
+theBoard = {'top-L':'', 'top-M':'', 'top-R':'', 
+            'mid-L':'','mid-M':'', 'mid-R':'',
+            'low-L':'', 'low-M':'', 'low-R':''}
+
+print ('Choose one Space per turn')
+print()
+print(' top-L'+' top-M'+ ' top-R')
+print()   
+print(' mid-L'+' mid-M'+ ' mid-R')  
+print()
+print(' low-L'+' low-M'+ ' low-R ')          
+print()
+
+# FUNÇÃO PARA PRINTAR A CERQUILHA NA TELA
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] +'|' + board['top-R'])
+    print('+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+# O JOGO INICIA PELA VEZ DO 'X'
+
+turn = 'X'
+
+for i in range(9):  # O LAÇO OCORRERÁ 9 VEZES
+    printBoard(theBoard) # CHAMA A FUNÇÃO PARA PRINTAR A CERQUILHA
+    print('Turn for ' + turn + '. Move on wich space? ') # INDICA A VEZ DO JOGADOR
+    move = input() # O JOGADOR DEVERÁ COLOCAR A POSIÇÃO QUE QUER JOGAR
+    theBoard[move] = turn # ASSOCIA A JOGADA AO JOGADOR
+    print()
+    # CONDICIONAL PARA REALIZAR A MUDANÇA DE JOGADOR
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+printBoard(theBoard) # PRINTA NA TELA A CEQUILHA PREENCHIDA
