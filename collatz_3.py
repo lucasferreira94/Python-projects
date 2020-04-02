f521fd455de6fbff629550af55e4f940e7c3950a
Lucas Santos Ferreira <45995734+lucasferreira94@users.noreply.github.com>
2020-04-02 10:06:38 -0300

    Update collatz_3.py

--- collatz_3.py
+++ collatz_3.py
@@ -4,12 +4,8 @@ def collatz(number):
         elif(number%2 == 1):
             number = ((number*3)+1)
         return int(number)
-
-# INICIANDO A VARIÁVEL COM '0' PARA NÃO HAVER POSSÍVEIS INTERFERÊNCIAS DE MEMÓRIA
 cont = 0
 
-# O LAÇO ABAIXO SE TRATA DE UM TRATAMENTO DE ERRO, PARA EVITAR O
-# 'INPUT' DE UMA STRING AO INVÉS DE UM NÚMERO INTEIRO
 while True:
     try:
         print()
@@ -20,20 +16,20 @@ while True:
         print('Necessário informar um NÚMERO INTEIRO!!')
 
 while True:
-    numero = collatz(numero) # O NÚMERO DIGITADO IRÁ RECEBER O CÁLCULO DA FUNÇÃO
+    numero = collatz(numero) 
     while True:
-        try: # NOVAMENTE UM TRATAMENTO DE ERRO, IGUAL AO DE CIMA
+        try: 
             print()
             cont = int(input('Deseja continuar? (1) SIM ou (2) NÃO: '))
             break       
         except ValueError as err:
             print('Por gentileza, digite (1) se deseja continuar, ou (2) se deseja encerrar o programa')
 
-    if(cont == 1): # SE DIGITADO '1', IRÁ CONTINUAR O CÁLCULO
-        collatz(numero) # CHAMA NOVAMENTE A FUNÇÃO E FAZ O CÁLCULO
-        print(numero) # EXIBE O NOVO RESULTADO
-        if(numero == 1): # SE O NÚMERO CALCULADO ATINGIR O RESULTADO '1', ENCERRA O PROGRAMA
+    if(cont == 1):
+        collatz(numero)
+        print(numero) 
+        if(numero == 1): 
              break
-    elif(cont == 2): # IGNORANDO A CONDIÇÃO ACIMA; SE DIIGTADO '2' O PROGRAMA É ENCERRADO APÓS O ÚLTIMO CÁLCULO SOLICITADO
+    elif(cont == 2): 
         break 
     
